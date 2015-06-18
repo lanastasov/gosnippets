32da282b0ac17bff2bdfde886aef1b37546b59e5
unknown <lubomir.anastasov@gmail.com>
2015-06-18 13:32:16 +0300

    main

--- gosnippets.go
+++ gosnippets.go
@@ -4,7 +4,7 @@ import (
 	"os"
 	"os/exec"
 	// "path"
-	// "fmt"
+	"fmt"
 )
 
 func ScreenCls() {
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"github.com/lanastasov/gosnippets"
+)
+
+func main() {
+	fmt.Println("----")
+}
