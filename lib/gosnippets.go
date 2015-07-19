2398f8a51cca42959c3665534df69a466f358d1e
Lubomir Anastasov <lubomir.anastasov@gmail.com>
2015-07-19 19:07:50 +0300

    test for equality

--- lib/gosnippets.go
+++ lib/gosnippets.go
@@ -83,3 +83,17 @@ func Closure() int {
 
 	return 0
 }
+
+func TestEq(a, b []int) bool {
+	if len(a) != len(b) {
+		return false
+	}
+
+	for i := range a {
+		if a[i] != b[i] {
+			return false
+		}
+	}
+
+	return true
+}
