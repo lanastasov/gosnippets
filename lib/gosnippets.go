0f7a41df5892008baee24f5a04ad81847ee1b31a
lanastasov <lubomir.anastasov@gmail.com>
2019-11-20 03:06:25 +0200

    Update gosnippets.go

--- lib/gosnippets.go
+++ lib/gosnippets.go
@@ -195,3 +195,21 @@ func RemoveDuplicatesFromSlice(s []string) []string {
       }
       return result
 }
+
+func ReplaceNumbersByZeroFromString () {
+	str := "abc123465@ahsjf222"
+    newStr := make([]rune, len(str))
+    i, added := 0, false
+    for _, r := range str {
+        if r >= '0' && r <= '9' {
+            if added {
+                continue
+            }
+            added, newStr[i] = true, '0'
+        } else {
+            added, newStr[i] = false, r
+        }
+        i++
+    }
+    fmt.Println(string(newStr[:i]))
+}
